7d4ee12053e14a69d11c54bd734bbb13a5eedd6f
NishthaBordia <bordianishtha@gmail.com>
2025-05-14 12:16:38 +0530

    navbarEdit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,66 +9,65 @@ const Navbar = () => {
   return (
     <nav
       style={{
-        padding: "1rem",
-        backgroundColor: "#333",
-        color: "#fff",
+       
+         paddingTop: ".5rem",
+    paddingBottom: ".5rem",
+    paddingLeft: "1rem",
+    paddingRight:"1rem",
+        backgroundColor:"#340646",
+        // color: "#fff",
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
-      }}
+
+         
+      }} className="mainNavbar"
     >
+    <ul style={{ listStyleType: "none", gap: "20px" }}><li><img src="/primaryLogo2.png" alt="Auralis logo" className="navbarLogo"></img></li></ul>
       <ul
         style={{ listStyleType: "none", gap: "20px", display: "flex" }}
         className="navbar-list"
       >
         <li>
-          <Link to="/" style={{ color: "#fff", textDecoration: "none" }}>
+          <Link to="/" style={{ textDecoration: "none" }} className="navbarTitles">
             Home
           </Link>
         </li>
         <li>
           <Link
             to="/gen-music"
-            style={{ color: "#fff", textDecoration: "none" }}
-          >
+            style={{  textDecoration: "none" }}
+           className="navbarTitles">
             Generate Music
           </Link>
         </li>
         <li>
           <Link
             to="/sample-music"
-            style={{ color: "#fff", textDecoration: "none" }}
-          >
+            style={{  textDecoration: "none" }}
+           className="navbarTitles">
             Sample Music
           </Link>
         </li>
        
         <li>
-          <Link to="/faq" style={{ color: "#fff", textDecoration: "none" }}>
+          <Link to="/faq" style={{ textDecoration: "none" }} className="navbarTitles">
             FAQ
           </Link>
         </li>
         <li>
-          <Link to="/account" style={{ color: "#fff", textDecoration: "none" }}>
+          <Link to="/account" style={{ textDecoration: "none" }} className="navbarTitles">
             Account
           </Link>
         </li>
         <li>
-          <Link to="/blog" style={{ color: "#fff", textDecoration: "none" }}>
+          <Link to="/blog" style={{ textDecoration: "none" }} className="navbarTitles">
             Blog
           </Link>
         </li>
       </ul>
       <ul style={{ listStyleType: "none", gap: "20px" }}>
-        {/* <li><Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>Log In</Link></li> */}
-        {authStatus ? (
-          <li>Logout</li>
-        ) : (
-          <>
-            <li>Login</li>
-            <li>Register</li>
-          </>
-        )}
+     <li className="navbarTitles">Register </li>
       </ul>
     </nav>
   )
